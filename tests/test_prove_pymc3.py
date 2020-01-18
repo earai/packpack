94b966e9a64e7f4e3da3de51946fcb339a6b8001
Michael Cannamela <m.cannamela@gmail.com>
2020-01-18 18:07:01 -0500

    add pytest and prove we can sample a simple model

---
+++ tests/test_prove_pymc3.py
@@ -0,0 +1,11 @@
+from packpack.prove_pymc3 import build_proof_model
+import pymc3 as pm
+
+
+def test_proof_model():
+    """Just prove it runs"""
+    m = build_proof_model()
+    with m:
+        trace = pm.sample(50)
+    print("blah")
+
