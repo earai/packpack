5e699781d7c56d0cf07f04b8144b12a135375955
erin <erin.arai@gmail.com>
2020-01-20 15:23:51 -0500

    test to make sure we can instantiate our RV based dataclasses

---
+++ tests/dataclasses/test_mace.py
@@ -0,0 +1,14 @@
+import pymc3 as pm
+
+from packpack.dataclasses import Mace
+
+
+def test_mace():
+    """Test that we can initialize a Mace"""
+    with pm.Model() as model:
+        mace = Mace(eye_sting=pm.Exponential('eye_sting', 1.0), nose_burn=pm.Exponential('nose_burn', 1.0))
+
+    sting = mace.eye_sting.random()
+    burn = mace.nose_burn.random()
+    assert sting > 0
+    assert burn > 0
