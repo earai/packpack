cbe3da77f208ce07dd31ad093c995c9d98f3e4a0
erin <erin.arai@gmail.com>
2020-01-20 18:13:26 -0500

    refactor plotting

--- packpack/tool_threat_interaction/bear_mace_model_runner.py
+++ packpack/tool_threat_interaction/bear_mace_model_runner.py
@@ -1,6 +1,8 @@
 import pymc3 as pm
 from matplotlib import pyplot as plt
 import numpy as np
+from pandas.plotting import scatter_matrix
+import pandas as pd
 
 class BearMaceModelRunner(object):
 
@@ -13,47 +15,44 @@ class BearMaceModelRunner(object):
         m = self.bear_mace_model.build_model()
         with m:
             self.prior_samples = pm.sample_prior_predictive(10000, m)
+            self.prior_samples_as_df = pd.DataFrame(self.prior_samples)
             self.trace = pm.sample(n_samples, tune=n_tuning_samples)
+            self.trace_as_df = pm.trace_to_dataframe(self.trace)
         return self.trace
 
+    def plot_marginal(self, variable_name):
+        var_pri = self.prior_samples[variable_name]
+        var_post = self.trace_as_df[variable_name]
+
+        def bins(x_prior, x_posterior, n=200):
+            return np.linspace(min(np.min(x_prior), np.min(x_posterior)), max(np.max(x_prior), np.max(x_posterior)), 200)
+
+        these_bins = bins(var_pri, var_post)
+
+        plt.hist(var_pri, these_bins, alpha=.4, label='prior', density=True)
+        plt.hist(var_post, these_bins, alpha=.4, label='posterior', density=True)
+        plt.xlabel(variable_name)
+        plt.legend()
+
+
     def visualize_trace(self):
-        df = pm.trace_to_dataframe(self.trace)
+        df = self.trace_as_df
         bear = self.bear_mace_model.bear
         mace = self.bear_mace_model.mace
         environment = self.bear_mace_model.environment
         encounter_result = self.bear_mace_model.encounter_result
 
-        bear_sight_pri = self.prior_samples['sight']
-        bear_sight_post = df.sight
+        plt.figure(figsize=(12,12))
+        keys = set(df.columns).intersection(set(self.prior_samples.keys()))
+        for i,k in enumerate(keys):
+            plt.subplot(2, 3, i+1)
+            self.plot_marginal(k)
 
-        def bins(x_prior, x_posterior, n=200):
-            return np.linspace(min(np.min(x_prior), np.min(x_posterior)), max(np.max(x_prior), np.max(x_posterior)), 200)
-
-        bear_bins = bins(bear_sight_pri, bear_sight_post)
-
-        plt.figure(figsize=(12, 12))
-
-        plt.subplot(2, 2, 1)
-        plt.hist(bear_sight_pri, bear_bins, alpha=.4, label='prior', density=True)
-        plt.hist(bear_sight_post, bear_bins, alpha=.4, label='posterior', density=True)
-        plt.xlabel('bear sight')
-        plt.legend()
+        scatter_matrix(self.trace_as_df, figsize=(12,12), alpha=.05)
+        plt.title("Posterior")
 
 
-        # plt.subplot(2, 2, 3)
-        # #plt.plot(jitter(n_items_pri), item_size_pri, 'k.', alpha=.04, label='prior')
-        # plt.xlabel('n_items (jittered for display purposes)')
-        # plt.ylabel('item_size')
-        # plt.legend()
-        #
-        # plt.subplot(2, 2, 4)
-        # #plt.plot(jitter(n_items_post), item_size_post, 'k.', alpha=.04, label='posterior')
-        # plt.xlabel('n_items ')
-        # plt.ylabel('item_size')
-        # plt.legend()
+        scatter_matrix(self.prior_samples_as_df, figsize=(12,12), alpha=.05)
+        plt.title("Prior")
 
-        # plt.plot()
-        # scatter_matrix(df, figsize=(12, 12))
         plt.show()
-        print('stop')
-
