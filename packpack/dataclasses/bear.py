36b2eaefef8e9458c3f63bd98b04f44579b71d83
erin <erin.arai@gmail.com>
2020-01-20 14:41:39 -0500

    Adding necessary dataclasses for bear-mace example

---
+++ packpack/dataclasses/bear.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Bear:
+    """Bear threat"""
+
+    sight: float
+    smell: float
---
+++ packpack/dataclasses/mace.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Mace:
+    """Mace tool"""
+
+    eye_sting: float
+    nose_burn: float
+
