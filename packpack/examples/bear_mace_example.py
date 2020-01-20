3b43f346b971e58f20a549df962aab01d00618bd
erin <erin.arai@gmail.com>
2020-01-20 18:51:56 -0500

    rearrange order to show how easy it is to swap out models

--- packpack/examples/bear_mace_example.py
+++ packpack/examples/bear_mace_example.py
@@ -13,11 +13,14 @@ def make_model(bear_mace_interaction):
 def main():
 
     crude_bear_mace_model = make_model(BearMaceModel.crude_bear_mace_interaction)
+    fancy_bear_mace_model = make_model(BearMaceModel.fancy_bear_mace_interaction)
+
+
     crude_bear_mace_runner = BearMaceModelRunner(crude_bear_mace_model)
     crude_bear_mace_runner.run_model(4000, 12000)
     crude_bear_mace_runner.visualize_trace()
 
-    fancy_bear_mace_model = make_model(BearMaceModel.fancy_bear_mace_interaction)
+
     fancy_bear_mace_runner = BearMaceModelRunner(fancy_bear_mace_model)
     fancy_bear_mace_runner.run_model(4000, 12000)
     fancy_bear_mace_runner.visualize_trace()
