d9271804a88517984dbb057460338a8f87cba47f
erin <erin.arai@gmail.com>
2020-01-20 17:14:58 -0500

    Make bear mace model run

--- packpack/tool_threat_interaction/bear_mace_model.py
+++ packpack/tool_threat_interaction/bear_mace_model.py
@@ -4,25 +4,38 @@ from packpack.datamodels import Bear, Environment, Mace, EncounterResult
 from typing import Callable
 import theano.tensor as tt
 
+
 class BearMaceModel(object):
 
     def __init__(self,
-                 environment: Environment,
-                 mace: Mace,
+                 make_environment: Callable[[], Environment],
+                 make_mace: Callable[[], Mace],
                  make_bear: Callable[[Environment], Bear],
-                 bear_mace_interaction: Callable[[Bear, Mace], EncounterResult]):
-        self.environment = environment
-        self.mace = mace
+                 make_encounter_result: Callable[[Bear, Mace], EncounterResult]):
+        self.make_environment = make_environment
+        self.make_mace = make_mace
         self.make_bear = make_bear
-        self.bear_mace_interaction = bear_mace_interaction
+        self.make_encounter_result = make_encounter_result
         self.bear = None
+        self.environment = None
+        self.mace = None
         self.encounter_result = None
 
     def build_model(self) -> Model:
         with Model() as model:
-            self.bear = self.make_bear(self.environment)
-            self.encounter_result = self.bear_mace_interaction(self.bear, self.mace)
+            self.bear = self.make_bear(self.make_environment())
+            self.encounter_result = self.make_encounter_result(self.bear, self.make_mace())
         return model
+
+    @classmethod
+    def make_environment(cls):
+        return Environment(latitude=pm.Uniform('latitude', -90., 90.))
+
+    @classmethod
+    def make_mace(cls):
+        eye_sting = pm.Uniform('eye_sting', 0., 1.)
+        nose_burn = pm.Deterministic('nose_burn', 1.-eye_sting)
+        return Mace(eye_sting=eye_sting, nose_burn= nose_burn)
 
     @classmethod
     def make_bear(cls, environment: Environment) -> Bear:
@@ -34,13 +47,13 @@ class BearMaceModel(object):
     @classmethod
     def crude_bear_mace_interaction(cls, bear: Bear, mace: Mace):
         logit_p = mace.nose_burn+mace.eye_sting-bear.smell-bear.sight
-        encounter_result = pm.Bernoulli('encounter_result', logit_p=logit_p, observed=1.)
-        return EncounterResult(encounter_result=encounter_result)
+        survived = pm.Bernoulli('survived', logit_p=logit_p, observed=1.)
+        return EncounterResult(survived=survived)
 
     @classmethod
     def fancy_bear_mace_interaction(cls, bear: Bear, mace: Mace):
         logit_p_nose = mace.nose_burn-bear.smell
         logit_p_eyes = mace.eye_sting-bear.sight
         logit_p = tt.max(logit_p_eyes, logit_p_nose)
-        encounter_result = pm.Bernoulli('encounter_result', logit_p=logit_p, observed=1.)
-        return EncounterResult(encounter_result=encounter_result)
+        survived = pm.Bernoulli('survived', logit_p=logit_p, observed=1.)
+        return EncounterResult(survived=survived)
--- packpack/tool_threat_interaction/bear_mace_model_runner.py
+++ packpack/tool_threat_interaction/bear_mace_model_runner.py
@@ -1,15 +1,59 @@
 import pymc3 as pm
+from matplotlib import pyplot as plt
+import numpy as np
 
 class BearMaceModelRunner(object):
 
     def __init__(self, bear_mace_model):
         self.bear_mace_model = bear_mace_model
+        self.prior_samples = None
+        self.trace = None
 
     def run_model(self, n_samples, n_tuning_samples):
-        m = self.bear_mace_model
+        m = self.bear_mace_model.build_model()
         with m:
-            trace = pm.sample(n_samples, tune=n_tuning_samples)
-        print(trace.report.ok)
+            self.prior_samples = pm.sample_prior_predictive(10000, m)
+            self.trace = pm.sample(n_samples, tune=n_tuning_samples)
+        return self.trace
+
+    def visualize_trace(self):
+        df = pm.trace_to_dataframe(self.trace)
+        bear = self.bear_mace_model.bear
+        mace = self.bear_mace_model.mace
+        environment = self.bear_mace_model.environment
+        encounter_result = self.bear_mace_model.encounter_result
+
+        bear_sight_pri = self.prior_samples['sight']
+        bear_sight_post = df.sight
+
+        def bins(x_prior, x_posterior, n=200):
+            return np.linspace(min(np.min(x_prior), np.min(x_posterior)), max(np.max(x_prior), np.max(x_posterior)), 200)
+
+        bear_bins = bins(bear_sight_pri, bear_sight_post)
+
+        plt.figure(figsize=(12, 12))
+
+        plt.subplot(2, 2, 1)
+        plt.hist(bear_sight_pri, bear_bins, alpha=.4, label='prior', density=True)
+        plt.hist(bear_sight_post, bear_bins, alpha=.4, label='posterior', density=True)
+        plt.xlabel('bear sight')
+        plt.legend()
 
 
+        # plt.subplot(2, 2, 3)
+        # #plt.plot(jitter(n_items_pri), item_size_pri, 'k.', alpha=.04, label='prior')
+        # plt.xlabel('n_items (jittered for display purposes)')
+        # plt.ylabel('item_size')
+        # plt.legend()
+        #
+        # plt.subplot(2, 2, 4)
+        # #plt.plot(jitter(n_items_post), item_size_post, 'k.', alpha=.04, label='posterior')
+        # plt.xlabel('n_items ')
+        # plt.ylabel('item_size')
+        # plt.legend()
+
+        # plt.plot()
+        # scatter_matrix(df, figsize=(12, 12))
+        plt.show()
+        print('stop')
 
