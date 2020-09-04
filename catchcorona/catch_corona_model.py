a4560e3aef445c4b145a410f84034a9aa5dae2cc
erin <erin.arai@gmail.com>
2020-09-03 21:53:18 -0400

    sketch for catch corona

---
+++ catchcorona/catch_corona_model.py
@@ -0,0 +1,64 @@
+from pymc3.model import Model, Deterministic
+import pymc3 as pm
+from packpack.datamodels import Bear, Environment, Mace, EncounterResult
+from typing import Callable
+import theano.tensor as tt
+import numpy as np
+
+
+class CatchCoronaModel(object):
+
+    def __init__(self,
+                 make_environment: Callable[[], Environment],
+                 make_mace: Callable[[], Mace],
+                 make_bear: Callable[[Environment], Bear],
+                 make_encounter_result: Callable[[Bear, Mace], EncounterResult]):
+        self.make_environment = make_environment
+        self.make_mace = make_mace
+        self.make_bear = make_bear
+        self.make_encounter_result = make_encounter_result
+        self.bear = None
+        self.environment = None
+        self.mace = None
+        self.encounter_result = None
+
+    def build_model(self) -> Model:
+        with Model() as model:
+            self.bear = self.make_bear(self.make_environment())
+            self.encounter_result = self.make_encounter_result(self.bear, self.make_mace())
+        return model
+
+    @classmethod
+    def make_simple_environment(cls):
+        return Environment(latitude=pm.Uniform('latitude', -90., 90.))
+
+    @classmethod
+    def make_simple_mace(cls):
+        eye_sting = pm.Uniform('eye_sting', 0., 1.)
+        nose_burn = pm.Deterministic('nose_burn', .5-eye_sting**2)
+        return Mace(eye_sting=eye_sting, nose_burn= nose_burn)
+
+    @classmethod
+    def make_simple_bear(cls, environment: Environment) -> Bear:
+        latitude = environment.latitude
+        deg_to_rad = np.pi/180.
+        sight = Deterministic('sight', tt.sin(latitude*deg_to_rad)**2.)
+        smell = Deterministic('smell', tt.cos(latitude*deg_to_rad)**2.)
+        return Bear(sight=sight, smell=smell)
+
+    @classmethod
+    def crude_bear_mace_interaction(cls, bear: Bear, mace: Mace):
+        stiffness=20.
+        logit_p = stiffness*(mace.nose_burn+mace.eye_sting-bear.smell-bear.sight)
+        survived = pm.Bernoulli('survived', logit_p=logit_p, observed=1.)
+        return EncounterResult(survived=survived)
+
+    @classmethod
+    def fancy_bear_mace_interaction(cls, bear: Bear, mace: Mace):
+        stiffness = 20.
+        logit_p_nose = (mace.nose_burn-bear.smell)*stiffness
+        logit_p_eyes = (mace.eye_sting-bear.sight)*stiffness
+        logit_p = tt.min(tt.stack([logit_p_nose, logit_p_eyes]))
+
+        survived = pm.Bernoulli('survived', logit_p=logit_p, observed=1.)
+        return EncounterResult(survived=survived)
--- catchcorona/examples/catch_corona_sketch.py
+++ catchcorona/examples/catch_corona_sketch.py
@@ -1,18 +1,34 @@
 from packpack.tool_threat_interaction.bear_mace_model import BearMaceModel
 from packpack.tool_threat_interaction.bear_mace_model_runner import BearMaceModelRunner
 from matplotlib import pyplot as plt
+import pymc3 as pm
+from pymc3.model import Model
+import numpy as np
+import theano.tensor as tt
 
 
-def build_model(self) -> Model:
-        with Model() as model:
-            self.bear = self.make_bear(self.make_environment())
-            self.encounter_result = self.make_encounter_result(self.bear, self.make_mace())
-        return model
+def build_model() -> Model:
+    n_patient = 10
+    n_sick = 2
+    n_vaccines = 2
+    n_well = int(n_patient - n_sick)
+    p_fp = .1
+    p_fn = .1
+    p_precede_sick = np.full(n_sick+1, 1. / (n_sick + 1.))
+    with Model() as model:
+        n_i = pm.Multinomial("well", n_well, p_precede_sick, shape=n_sick+1)
+        x_i = pm.Binomial("vaccines_wasted", n_i, p_fp, shape=n_sick+1)
+        y_j = pm.Bernoulli("failed_to_vaccinate", p_fn, shape=n_sick)
+        did_not_use_too_much_vaccine = (tt.sum(x_i[:-1]) <= n_vaccines - n_sick)
+        vaccinated_every_sickie = tt.prod(1. - y_j)
+        live = pm.Deterministic(name="alive", var=did_not_use_too_much_vaccine * vaccinated_every_sickie)
+    return model
+
 
 def main():
     """
     n_sick placed at random
-    i_0     is the index of the first sick patient eq if i=3, the number of nonsick patients precending the first
+    i_0     is the index of the first sick patient eq if i=3, the number of nonsick patients preceding the first
             sick patient is i_0
     n_vaccines expended before we get to the first sick person is:
                 x_0 ~ Binomial(i_0, P_fp) vaccines spent on non sick patients before encountering first sick patient
@@ -21,15 +37,16 @@ def main():
 
     y_j ~ Bernoulli(P_fn) - if we failed to vaccinate the jth sick patient
 
-    (sum_j(x_j) <= n_vaccines-n_sick)*product_j(1-y_j)  - if this is 1 then we live, 0 we die
+    * (sum_j(x_j) <= n_vaccines-n_sick)*product_j(1-y_j)  - if this is 1 then we live, 0 we die
+    * is a deterministic function of the three random variables x_i, y_j
+
+    Multinomial(n_patients-n_sick, (1/n_sick)*(ones_likes(n_patients)+1))
     """
-    bear_mace_model = make_model(BearMaceModel.fancy_bear_mace_interaction)
+    model = build_model()
+    x = pm.sampling.sample_prior_predictive(samples=100, model=model)
+    # print(x)
+    print(np.mean(x['alive']))
 
-    fancy_bear_mace_runner = BearMaceModelRunner(bear_mace_model)
-    fancy_bear_mace_runner.run_model(4000, 12000)
-    fancy_bear_mace_runner.visualize_trace()
 
-    plt.show()
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
