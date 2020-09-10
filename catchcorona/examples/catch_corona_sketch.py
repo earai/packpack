4ef27328dc879faeec5c7a8388df7e7e44f3005e
erin <erin.arai@gmail.com>
2020-09-09 22:50:30 -0400

    Working around shape hell

--- catchcorona/examples/catch_corona_sketch.py
+++ catchcorona/examples/catch_corona_sketch.py
@@ -1,26 +1,23 @@
-from packpack.tool_threat_interaction.bear_mace_model import BearMaceModel
-from packpack.tool_threat_interaction.bear_mace_model_runner import BearMaceModelRunner
-from matplotlib import pyplot as plt
-import pymc3 as pm
-from pymc3.model import Model
 import numpy as np
+import pymc3 as pm
 import theano.tensor as tt
+from pymc3.model import Model
 
 
 def build_model() -> Model:
-    n_patient = 10
-    n_sick = 2
-    n_vaccines = 2
+    n_patient = 1
+    n_sick = 1
+    n_vaccines = 1
     n_well = int(n_patient - n_sick)
-    p_fp = .1
-    p_fn = .1
+    p_fp = .01
+    p_fn = .35
     p_precede_sick = np.full(n_sick+1, 1. / (n_sick + 1.))
     with Model() as model:
-        n_i = pm.Multinomial("well", n_well, p_precede_sick, shape=n_sick+1)
-        x_i = pm.Binomial("vaccines_wasted", n_i, p_fp, shape=n_sick+1)
-        y_j = pm.Bernoulli("failed_to_vaccinate", p_fn, shape=n_sick)
-        did_not_use_too_much_vaccine = (tt.sum(x_i[:-1]) <= n_vaccines - n_sick)
-        vaccinated_every_sickie = tt.prod(1. - y_j)
+        n_i = pm.Multinomial("well", n_well, p_precede_sick, shape=(n_sick+1,))
+        x_i = pm.Binomial("vaccines_wasted", n_i, p_fp, shape=(n_sick+1,))
+        y_j = pm.Bernoulli("failed_to_vaccinate", p_fn, shape=(n_sick+1,))
+        did_not_use_too_much_vaccine = pm.Deterministic("did_not_use_too_much_vaccine", tt.sum(x_i[:-1]) <= n_vaccines - n_sick)
+        vaccinated_every_sickie = pm.Deterministic('vaccinated_every_sickie', tt.prod(1. - y_j[:-1]))
         live = pm.Deterministic(name="alive", var=did_not_use_too_much_vaccine * vaccinated_every_sickie)
     return model
 
@@ -42,9 +39,17 @@ def main():
 
     Multinomial(n_patients-n_sick, (1/n_sick)*(ones_likes(n_patients)+1))
     """
+    # with Model() as m:
+    #     y_j = pm.Normal("y", .1, .1, shape=1)
+    #     prod_y = pm.Normal('prod_y', 1.0-y_j , 1.0, shape=1)
+    #     # prod_y = pm.Deterministic('prod_y', tt.prod(tt.concatenate([tt.ones((1,)), tt.reshape(y_j, (1,))], axis=-1), keepdims=True))
+    # yy = pm.sampling.sample_prior_predictive(10, model=m)
+    #
+    # print(yy)
     model = build_model()
-    x = pm.sampling.sample_prior_predictive(samples=100, model=model)
-    # print(x)
+    x = pm.sampling.sample_prior_predictive(samples=5000, model=model)
+    print(x.keys())
+    print(x)
     print(np.mean(x['alive']))
 
 
